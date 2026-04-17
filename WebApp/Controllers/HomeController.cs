e46d52d968c2ac7c1c596244ee286b93fe7f6258
Alberto Jacobo Zamudio Chan <23470290@campeche.tecnm.mx>
2026-04-17 11:35:49 -0600

    Muestra información de un proyector usando un modelo de la vista

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -8,22 +8,34 @@ namespace WebApp.Controllers
     {
         public IActionResult Index()
         {
-            // Uso de ViewData / ViewBag
+            // Uso de ViewData / ViewBag / modelo de la vista
 
-                //ViewData["Id"] = 1;
-                //ViewData["Marca"] = "Epson";
-                //ViewData["Modelo"] = "XLight";
-                //ViewData["NumeroDeSerie"] = 123456;
-                //ViewData["Situacion"] = "Bueno";
-                //ViewData["FechaDeAlta"] = DateTime.Now.ToString("d");
+            //ViewData["Id"] = 1;
+            //ViewData["Marca"] = "Epson";
+            //ViewData["Modelo"] = "XLight";
+            //ViewData["NumeroDeSerie"] = 123456;
+            //ViewData["Situacion"] = "Bueno";
+            //ViewData["FechaDeAlta"] = DateTime.Now.ToString("d");
 
-                ViewBag.Id = 1;
-                ViewBag.Marca = "Epson";
-                ViewBag.Modelo = "XLight";
-                ViewBag.NumeroDeSerie = 123456;
-                ViewBag.Situacion = "Bueno";
-                ViewBag.FechaDeAlta = DateTime.Now.ToString("d");
-            return View();
+            //ViewBag.Id = 2;
+            //ViewBag.Marca = "Epson";
+            //ViewBag.Modelo = "XLight";
+            //ViewBag.NumeroDeSerie = 123456;
+            //ViewBag.Situacion = "Bueno";
+            //ViewBag.FechaDeAlta = DateTime.Now.ToString("d");
+
+            Proyectores proyector = new Proyectores()
+            {
+                Id = 1,
+                Marca = "Epson",
+                Modelo = "XLight",
+                NumeroDeSerie = "123456",
+                situacion = SituacionProyector.Bueno,
+                FechaDeAlta = DateTime.Now
+            };
+
+           
+            return View(proyector);
         }
 
         public IActionResult Privacy()
---
+++ WebApp/Models/Proyectores.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApp.Models
+{
+    public class Proyectores
+    {
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string NumeroDeSerie { get; set; }
+        public SituacionProyector situacion{ get; set; }
+        public DateTime FechaDeAlta { get; set; }
+        public DateTime? FechaDeBaja { get; set; }
+
+    }
+    public enum SituacionProyector
+    {
+        Bueno,
+        Regular,
+        Malo
+    }
+}
