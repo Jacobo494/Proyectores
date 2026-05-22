cf22a3155c3524dd8113bc6ff21ce35a38bd4888
Alberto Jacobo Zamudio Chan <23470290@campeche.tecnm.mx>
2026-05-22 10:33:40 -0600

    Agrega validacion personalizada

---
+++ WebApp/Validations/NoRepetirIdValidatorAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApp.Models;
+
+namespace WebApp.Validations
+{
+    public class NoRepetirIdValidatorAttribute : ValidationAttribute
+    {
+        private IList<Proyectores> _proyectores;
+        public NoRepetirIdValidatorAttribute(IList<Proyectores> proyectores)
+        {
+            _proyectores = proyectores;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                return null;
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
